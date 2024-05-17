b57a7d3054079419df76ce7e22a129ce27327c3b
Viktor1009 <viktorosterberg10@gmail.com>
2024-05-17 08:41:22 +0200

    First commit with default MAUI

---
+++ LayoutSystem/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace LayoutSystem
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ LayoutSystem/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace LayoutSystem
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
